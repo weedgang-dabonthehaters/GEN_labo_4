6ff03cbd774a49ca791880e00e1ba9beff92c81e
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2019-05-10 11:28:58 +0200

    Extract Function getOrders from OrdersWriter

--- src/main/java/ch/heigvd/gen2019/OrdersWriter.java
+++ src/main/java/ch/heigvd/gen2019/OrdersWriter.java
@@ -8,6 +8,10 @@ public class OrdersWriter {
     }
 
     public String getContents() {
+        return getOrders();
+    }
+
+    private String getOrders() {
         StringBuffer sb = new StringBuffer("{\"orders\": [");
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
